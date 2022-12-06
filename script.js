78b84999c1e27dc11c871cf7a789df8c25e37bf3
James Murphy <jamesivdeveloper@gmail.com>
2022-12-06 14:29:29 -0600

    update dom to display data retrieved from API and add functionality for buttons and add loading signal when fetching data

--- script.js
+++ script.js
@@ -1,14 +1,65 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const authorText = document.getElementById("author");
+const twitterBtn = document.getElementById("twitter");
+const newQuoteBtn = document.getElementById("new-quote");
+const loader = document.getElementById("loader");
+
+function loading() {
+  loader.hidden = false;
+  quoteContainer.hidden = true;
+}
+
+function complete() {
+  quoteContainer.hidden = false;
+  loader.hidden = true;
+}
+
+function newQuote() {
+  loading();
+  const quote = apiQuote;
+
+  if (!quote.author) {
+    authorText.textContent = "Unknown";
+  } else {
+    authorText.textContent = quote.author;
+  }
+
+  if (quote.content.length > 120) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+
+  quoteText.textContent = quote.content;
+  complete();
+}
+
 // fetch quote
 async function getQuote() {
+  loading();
   const apiUrl = "https://api.quotable.io/random";
 
   try {
     const response = await fetch(apiUrl);
     apiQuote = await response.json();
     console.log(apiQuote);
+    newQuote();
   } catch (error) {
     console.log(error);
   }
 }
+
+// tweet quote
+function tweetQuote() {
+  const twitterUrl = `https://twitter.com/intent/tweet?text=${quoteText.textContent} - ${authorText.textContent}`;
+  window.open(twitterUrl, "_blank");
+}
+
+// new quote event handler
+newQuoteBtn.addEventListener("click", getQuote);
+
+// tweet quote event listener
+twitterBtn.addEventListener("click", tweetQuote);
 
 getQuote();
