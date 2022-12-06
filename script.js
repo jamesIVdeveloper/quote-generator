a94782a43162d9641fcc6d0085916f27a44afa6b
James Murphy <jamesivdeveloper@gmail.com>
2022-12-06 13:32:36 -0600

    style buttons and add fetch call for random quote from Quotable API

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
+// fetch quote
+async function getQuote() {
+  const apiUrl = "https://api.quotable.io/random";
 
+  try {
+    const response = await fetch(apiUrl);
+    apiQuote = await response.json();
+    console.log(apiQuote);
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+getQuote();
